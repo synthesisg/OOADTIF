18997c6b946bac24986c8636cc6c41bdcb6f3f9e
synthesis_g <523029735@qq.com>
2018-12-04 13:46:12 +0800

    base admin finish

--- test/ooadtest4_5/ooadtest4_5/Controllers/AdminController.cs
+++ test/ooadtest4_5/ooadtest4_5/Controllers/AdminController.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 
 using ooadtest4_5.Models;
+
+/*
+ *  未身份验证
+ */
 namespace ooadtest4_5.Controllers
 {
     public class AdminController : Controller
@@ -35,65 +39,191 @@ namespace ooadtest4_5.Controllers
             }
             return View();
         }
+        public ActionResult Logout()
+        {
+            Session["is_admin"] = false;
+            return RedirectToAction("Login");
+        }
         public ActionResult TeacherInfo()
         {
-            /*
-            if (Session["is_admin"] == null) return Content("[null]Login First!");
-            if ((bool)Session["is_admin"] == false) return Content("[false]Login First!");
-            //*/
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
 
             if (Request.HttpMethod == "POST")
             {
-                int id = Convert.ToInt32(Request["newpass"]);
+                string sid = Request["newpass"];
+                int id = Convert.ToInt32(sid);
                 userinfo ui = uidb.data.Find(id);
                 string newpwd = Request["inputpwd_" + Request["newpass"]];
-                ui.password = newpwd;
-                uidb.SaveChanges();
-                Response.Write("<script type='text/javascript'>alert('Success!');</script>");
+                if (newpwd != null && newpwd != "")
+                {
+                    ui.password = newpwd;
+                    uidb.SaveChanges();
+                    Response.Write("<script type='text/javascript'>alert('Modify Success!');</script>");
+                }
             }
-            var teacher = from ui in uidb.data where (ui.is_student == false) select ui;
-            //return Content(teacher.Count().ToString());
-            ViewBag.teacherlist = teacher;
+
+            if (Request["Search"] != null)
+            {
+                string search = Request["Search"];
+                ViewBag.teacherlist = from ui in uidb.data where ((ui.is_student == false) && (ui.academic_id.Contains(search) || ui.name.Contains(search))) select ui;
+            }
+            else
+                ViewBag.teacherlist = from ui in uidb.data where (ui.is_student == false) select ui;
             return View();
+        }
+        public ActionResult TeacherDel(int id)
+        {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
+            userinfo ui = uidb.data.Find(id);
+            if (ui != null)
+            {
+                uidb.data.Remove(ui);
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert('Delete Success!');</script>");
+            }
+            return RedirectToAction("TeacherInfo");
         }
         public ActionResult TeacherInfoMod(int id)
         {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
             userinfo ui = uidb.data.Find(id);
             if (Request.HttpMethod=="POST")
             {
-                    ui.name = Request["name"];
-                    ui.academic_id = Request["academic_id"];
-                    ui.email = Request["email"];
-                    uidb.SaveChanges();
-                    Response.Write("<script type='text/javascript'>alert('Success!');</script>");
+                ui.name = Request["name"];
+                ui.academic_id = Request["academic_id"];
+                ui.email = Request["email"];
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert('Success!');</script>");
             }
             ViewBag.uimod = ui;
             return View();
         }
-        public string getstr()
-        {
-            //Response.Redirect("TeacherInfo");
-            //Response.Write("<script type='text/javascript'>alert('Success!');</script>");
-            string aa = "Muggle";
-            return Request["newpass"];
-        }
         public ActionResult TeacherCreate()
         {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
+            if (Request.HttpMethod=="POST")
+            {
+                userinfo ui = new userinfo
+                {
+                    name = Request["name"],
+                    academic_id = Request["academic_id"],
+                    email = Request["email"],
+                    password = Request["password"],
+                    is_student = false
+                };
+                uidb.data.Add(ui);
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert(' Create Success!');</script>");
+                return RedirectToAction("TeacherInfo");
+            }
             return View();
         }
+
         public ActionResult StudentInfo()
         {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
+            if (Request.HttpMethod == "POST")
+            {
+                string sid = Request["newpass"];
+                int id = Convert.ToInt32(sid);
+                userinfo ui = uidb.data.Find(id);
+                string newpwd = Request["inputpwd_" + Request["newpass"]];
+                if (newpwd != null && newpwd != "")
+                {
+                    ui.password = newpwd;
+                    uidb.SaveChanges();
+                    Response.Write("<script type='text/javascript'>alert('Modify Success!');</script>");
+                }
+            }
+            if (Request["Search"] != null)
+            {
+                string search = Request["Search"];
+                ViewBag.studentlist = from ui in uidb.data where ((ui.is_student == true) && (ui.academic_id.Contains(search) || ui.name.Contains(search))) select ui;
+            }
+            else
+                ViewBag.studentlist = from ui in uidb.data where (ui.is_student == true) select ui;
             return View();
         }
-        public ActionResult StudentInfoMod()
+        public ActionResult StudentInfoMod(int id)
         {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
+            userinfo ui = uidb.data.Find(id);
+            if (Request.HttpMethod == "POST")
+            {
+                ui.name = Request["name"];
+                ui.academic_id = Request["academic_id"];
+                ui.email = Request["email"];
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert('Success!');</script>");
+            }
+            ViewBag.uimod = ui;
             return View();
         }
         public ActionResult StudentCreate()
         {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
+
+            if (Request.HttpMethod == "POST")
+            {
+                userinfo ui = new userinfo
+                {
+                    name = Request["name"],
+                    academic_id = Request["academic_id"],
+                    email = Request["email"],
+                    password = Request["password"],
+                    is_student = true
+                };
+                uidb.data.Add(ui);
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert(' Create Success!');</script>");
+                return RedirectToAction("StudentInfo");
+            }
             return View();
         }
+        public ActionResult StudentDel(int id)
+        {
+            if (is_judge){
+                if (Session["is_admin"] == null || (bool)Session["is_admin"] == false)
+                    return Logout();
+            }
 
+            userinfo ui = uidb.data.Find(id);
+            if (ui != null)
+            {
+                uidb.data.Remove(ui);
+                uidb.SaveChanges();
+                Response.Write("<script type='text/javascript'>alert('Delete Success!');</script>");
+            }
+            return RedirectToAction("StudentInfo");
+        }
         userinfoDB uidb = new userinfoDB();
+        bool is_judge = false;
     }
 }
